a26cab24890fd60661be5c79ad839ba745e36a80
mohammedbellahcene <mohammedbellahcene95@gmail.com>
2021-12-16 16:52:22 +0100

    modal pagination

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ import { useState, useEffect } from "react";
 
 const App = () => {
   // ce state contiendra toutes les data récupérees par l'appel axios
+  const url2 = "https://api-allocine.herokuapp.com/api/movies/popular";
+  const url1 = "https://api-allocine.herokuapp.com/api/movies/upcoming";
+  const url3 = "https://api-allocine.herokuapp.com/api/movies/top_rated";
   const [data, setData] = useState(null);
+  const [page, setPage] = useState(1);
+  const [url, setUrl] = useState(url1);
+  const [onmodal, setOnmodal] = useState(false)
+  const [position, setPosition] = useState(-1)
+
+
+
 
   // * * * * * * * ASYNC / AWAIT * * * * * * *
   // on définit la fonction fetchData comme ASYNCHRONE avec le mot reservé async.
@@ -14,7 +24,7 @@ const App = () => {
   // On ne peut utiliser AWAIT QUE DANS UNE FONCTION DECLAREE AVEC ASYNC
   const fetchData = async () => {
     const response = await axios.get(
-      "https://api-allocine.herokuapp.com/api/movies/upcoming"
+      url
     );
     console.log(
       "DATAS RECUPEREES APRES ATTENTE DU AWAIT:",
@@ -35,33 +45,44 @@ const App = () => {
   // fetchData() => mise A jour du state data => rechargement de la page => fetchData() => mise A jour du state data => rechargement de la page etc ... etc ...
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [url]);
   const urlImgPrefix = "https://image.tmdb.org/t/p/w370_and_h556_bestv2";
 
   return (
-    <div  className="container">
-     {/*BONJOUR 
-      <span>PAGE</span>*/ } 
-      {/* Cette ternaire nous permet de n'afficher data QUE si il est rempli.
+    <div className="pageprincipal">
+      <div className="pagination"><button onClick={() => setUrl(url1)}>Url upcoming</button><button onClick={() => setUrl(url2)}>Url popular </button><button onClick={() => setUrl(url3)}>Url top rated </button><button onClick={() => !(page > 4) && setPage(page + 1)}>+</button><span>{page}</span><button onClick={() => !(page < 2) && setPage(page - 1)} >-</button></div>
+
+      <div className={onmodal ? "on" : "off"} ><img src={!(position === -1) && urlImgPrefix + data[position].backdrop_path} alt="" /></div>
+      <div className="container">
+
+
+
+        {/* Cette ternaire nous permet de n'afficher data QUE si il est rempli.
       Donc au chargement de la page, pendant 0.5 secondes d'attente de retour de l'appel axios, on affiche "en attente". En bonus, affichez une roue de chargement `a la place de ce "EN ATTENTE" */}
-      {data
-        ? data.map((film, i) => {
+        {data
+          ? data.map((film, i) => {
             // ne pas oublier d'associer une key à chaque element, meme si ça semble ne pas nous etre utile, sinon react nous sort un warning
             // i représente la position du film courant dans le tableau
-            return <div key={i} className="cardfilm">
-              
-              <div className="poster_path"><img src={urlImgPrefix+film.poster_path} alt=""/></div>
+
+            return <div key={i} className="cardfilm" onClick={() => { setOnmodal(true); setPosition(i) }} >
+
+
+              <div className="poster_path"><img src={urlImgPrefix + film.poster_path} alt="" /></div>
               <div className="description">
-                  <div className="original_title">{film.original_title}</div>
-                  <div className="release_date">{film.release_date}</div>
-              
-                  <div className="overview">{film.overview}</div>
+                <div className="original_title">{film.original_title}</div>
+                <div className="release_date">{film.release_date}</div>
+
+                <div className="overview">{film.overview}</div>
               </div>
 
-              </div>;
+
+            </div>;
+
           })
-        : "EN ATTENTE"}
+          : "EN ATTENTE"}
+      </div>
     </div>
+
   );
 };
 
